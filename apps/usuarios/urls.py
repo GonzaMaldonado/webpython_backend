a963d758821829d544c2c1c2751d4d2902826867
GonzaMaldonado <nahuel.maldonado.gonzalo@gmail.com>
2023-11-30 22:46:29 -0300

    feat: añadiendo urls para usuarios

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -1,3 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import Login, Register, Logout, UserViewSet
 
-urlpatterns = []
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='users')
+
+urlpatterns = [
+  path('login/', Login.as_view()),
+  path('register/', Register.as_view()),
+  path('logout/', Logout.as_view()),
+  path('refresh/', TokenRefreshView.as_view()),
+  path('', include(router.urls)),
+]
