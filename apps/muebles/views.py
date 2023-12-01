202753f99df71ba0e2fa5cfdcec16eff519d9d74
GonzaMaldonado <nahuel.maldonado.gonzalo@gmail.com>
2023-11-30 23:20:18 -0300

    feat: añadiendo lógica para muebles en views

--- apps/muebles/views.py
+++ apps/muebles/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-# Create your views here.
+from .serializers import MuebleSerializer
+
+
+class MueblesListView(generics.ListAPIView):
+  """
+  Permite ver una lista completa de todos los Muebles
+  """
+  queryset = MuebleSerializer.Meta.model.objects.all()
+  serializer_class = MuebleSerializer
+
+
+class MuebleDetailView(generics.RetrieveAPIView):
+  """
+  Permite ver el detalle de un Mueble
+  """
+  queryset = MuebleSerializer.Meta.model.objects.all()
+  serializer_class = MuebleSerializer
+
+
+class MuebleViewSet(viewsets.ModelViewSet):
+  """
+  Permite hacer un CRUD sobre el modelo Mueble(solo para usuarios administradores)
+  """
+  permission_classes = [IsAuthenticated, IsAdminUser]
+  queryset = MuebleSerializer.Meta.model.objects.all()
+  serializer_class = MuebleSerializer
