3b9601e1de2fbd5530c8fc60ad6f5d124829586f
GonzaMaldonado <nahuel.maldonado.gonzalo@gmail.com>
2023-11-30 23:01:13 -0300

    feat: añadiendo modelo Mueble

--- apps/muebles/admin.py
+++ apps/muebles/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Mueble
 
-# Register your models here.
+admin.site.register(Mueble)
--- apps/muebles/models.py
+++ apps/muebles/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Mueble(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+  nombre = models.CharField(max_length=50)
+  imagen = models.ImageField(upload_to='muebles/')
+  descripcion = models.TextField(blank=True, null=True)
+  color = models.CharField(max_length=100, blank=True, null=True)
+  medidas = models.CharField(max_length=100, blank=True, null=True)
+  precio = models.DecimalField(max_digits=12, decimal_places=2)
+  
+  def __str__(self):
+    return self.nombre
