90c59557ef18114ed1b5a0767e54151d8cb7c04c
GonzaMaldonado <nahuel.maldonado.gonzalo@gmail.com>
2023-11-30 23:24:34 -0300

    feat: añadiendo urls para muebles

--- apps/muebles/urls.py
+++ apps/muebles/urls.py
@@ -1,3 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from .views import MueblesListView, MuebleDetailView, MuebleViewSet
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = []
+router = DefaultRouter()
+router.register(r'muebles', MuebleViewSet, basename='muebles')
+
+urlpatterns = [
+  path('all_muebles/', MueblesListView.as_view()),
+  path('mueble/<int:pk>/', MuebleDetailView.as_view()),
+  path('', include(router.urls)),
+]
--- apps/muebles/views.py
+++ apps/muebles/views.py
@@ -1,5 +1,4 @@
-from rest_framework import viewsets, generics, status
-from rest_framework.response import Response
+from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from .serializers import MuebleSerializer
