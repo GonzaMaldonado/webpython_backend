75448b821b6607f2f218573804b209201285a5ae
GonzaMaldonado <nahuel.maldonado.gonzalo@gmail.com>
2023-11-30 22:40:49 -0300

    feat: añadiendo lógica de user en views

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from rest_framework import viewsets, views, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-# Create your views here.
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .models import User
+from .serializers import MyTokenObtainPairSerializer, RegisterSerializer, UserSerializer
+
+
+class Login(TokenObtainPairView):
+  serializer_class = MyTokenObtainPairSerializer
+
+
+class Register(views.APIView):
+  serializer_class = RegisterSerializer
+
+  def post(self, request, *args, **kwargs):
+    user_serializer = self.serializer_class(data=request.data)
+
+    if user_serializer.is_valid():
+      user_serializer.save()
+      login = MyTokenObtainPairSerializer(data=request.data)
+      if login.is_valid():
+        return Response({
+          'access': login.validated_data.get('access'),
+          'refresh': login.validated_data.get('refresh'),
+          'user': user_serializer.data,
+          'message': 'Usuario creado correctamente'
+        }, status=status.HTTP_201_CREATED)
+      
+    return Response({'message': 'Existen errores en el registro', 'error': user_serializer.errors
+                         },status=status.HTTP_400_BAD_REQUEST)
+
+
+class Logout(views.APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+    
+    def post(self, request, *args, **kwargs):
+        user = User.objects.filter(id=request.data.get('user', 0))
+
+        if user.exists():
+            RefreshToken.for_user(user.first())
+            return Response({'message': 'Sesión cerrada exitosamente'}, status=status.HTTP_204_NO_CONTENT)
+        return Response({'message': 'Credenciales invalidas'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    queryset = User.objects.filter(is_active=True)
+    serializer_class = UserSerializer
+
+    def destroy(self, request, pk=None):
+        user = User.objects.filter(id=pk).update(is_active=False)
+        if user == 1:
+            return Response({'message': 'Usuario eliminado correctamente'}, status=status.HTTP_204_NO_CONTENT)
+        return Response({'error': 'Usuario no encontrado'}, status=status.HTTP_404_NOT_FOUND)
